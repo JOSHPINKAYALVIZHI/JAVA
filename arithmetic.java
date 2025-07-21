9ee8a9bde00a5413b43227d775bb32dfa18cd2b0
JOSHPINKAYALVIZHI <joshpinkayalvizhi1247@gmail.com>
2025-07-21 23:36:57 +0530

    arithmetic

---
+++ arithmetic.java
@@ -0,0 +1,15 @@
+public class arithmetic {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        int diff = num1 - num2;
+        int mul = num1 * num2;
+        int div = num2 / num1;
+        System.out.println("sum = "+sum);
+        System.out.println("diff = "+diff);
+        System.out.println("mul = "+mul);
+        System.out.println("div = "+div);
+
+    }
+}
